92f4c17931fef0b9688d2424b7ee0fd1df42676b
jamietopp21 <jamietopp8@hotmail.com>
2018-07-10 15:42:15 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import {NameGen} from './namegen/NameGen'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">TOOLKIT</h1>
+        </header>
+        <NameGen/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/namegen/NameGen.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import basetoblob from 'b64-to-blob';
+
+
+export class NameGen extends React.Component{
+    state = {
+        fileUrl: null,
+        generatedName: null,
+        loading: false,
+    }
+
+    handleForm(e){
+        e.preventDefault();
+        this.setState({ loading: true });
+        const name = document.querySelector('.name').value
+        fetch('https://vlp2rp5ld3.execute-api.eu-west-1.amazonaws.com/api/?name='+name)
+            .then((res) => {
+                return res.json()
+                
+            })
+            .then((data) => {
+                const blob = basetoblob(data.image,'image/png');
+                const blobUrl = URL.createObjectURL(blob);
+                
+                this.setState({
+                    loading: false,
+                    generatedName: name,
+                    fileUrl: blobUrl,
+                });
+
+                console.log(data)
+            })
+    }
+
+    render(){
+        const { fileUrl, generatedName, loading } = this.state;
+
+        return(
+            <div>
+                <h1>Name Gen</h1>
+                <form className="form" onSubmit={this.handleForm.bind(this)}>
+                    <input type="text" className="name"/>
+                    <input type="submit" value="Generate" />
+                </form>
+                {loading ? <h2>Loading!</h2> : null}
+                <a href={fileUrl} download disabled={fileUrl === null}>Download {generatedName}</a>
+            </div>
+        )
+    }
+}
